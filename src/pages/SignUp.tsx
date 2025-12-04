299441c41f8cb76aebed5fa1406ff0b2fd9a59d8
FIRST_NAME LAST_NAME <julpaultes@gmail.com>
2025-12-04 15:24:58 +0100

    adding logo and text in sign up

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -4,6 +4,11 @@ export default function SignUp() {
 	return (
 		<div className="signup-card">
 			<p className="signup-error">Soon available</p>
+			<img
+				alt="formateurs"
+				src="src/assets/images/formateurs.png"
+				className="signup-image"
+			/>
 		</div>
 	);
 }
