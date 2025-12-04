01d23fbf0bcbd342becd3abf4b7a2fdc4d96c316
FIRST_NAME LAST_NAME <julpaultes@gmail.com>
2025-12-04 15:15:52 +0100

    removing Husky

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -3,12 +3,7 @@ import "../styles/SignUp.css";
 export default function SignUp() {
 	return (
 		<div className="signup-card">
-			<p className="signup-error">La fameuse erreur 404 !!!</p>
-			<img
-				alt="formateurs"
-				src="src/assets/images/formateurs.png"
-				className="signup-image"
-			/>
+			<p className="signup-error">Soon available</p>
 		</div>
 	);
 }
